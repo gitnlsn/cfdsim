d81f92be70b4bdf4622879a30cb48ce9a479708b
myTuringMachine <nelson.tamashiro.usp@gmail.com>
2017-09-02 11:14:52 -0300

    ''

---
+++ updateGit.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+THIS_NAME="Update Git v0.01"
+COMMIT_DESCRIPTION=$*
+
+echo "${THIS_NAME}: begin."
+
+git add .
+git commit -m \'"${COMMIT_DESCRIPTION}"\'
+git push origin master
+#echo \'"${COMMIT_DESCRIPTION}"\'
+
+
+echo "${THIS_NAME}: end."
