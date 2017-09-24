904e3593b1063939cc45df2603f9de8e18b6f2e2
myTuringMachine <nelson.tamashiro.usp@gmail.com>
2017-09-24 19:05:00 -0300

    'new Lagrangian Formulation'

--- 004.Separation_Lagrange_try2.py
+++ 004.Separation_Lagrange_try2.py
@@ -9,13 +9,13 @@ from fenics import *
 from mshr   import *
 
 cons_vin = 1.0E-2
-cons_rh1 = 1.0E+3
-cons_rh2 = 1.0E+3
-cons_mu1 = 1.0E-3
-cons_mu2 = 1.0E-3
+cons_rh1 = 1.0E+0
+cons_rh2 = 1.0E+0
+cons_mu1 = 1.0E-0
+cons_mu2 = 1.0E-0
 cons_gg  = 0.0E-1
 
-mesh_res = 100
+mesh_res = 50
 mesh_P0  = 0.0
 mesh_LL  = 3.0E-2
 mesh_DD  = 1.0E-2
@@ -28,35 +28,22 @@ mesh = generate_mesh(channel, mesh_res)
 
 inlet  = '(x[0]=='+str(mesh_P0)+')'
 walls  = '(x[1]=='+str(mesh_P0)+') || (x[1]=='+str(mesh_DD)+')'
-obsts  = '(on_boundary && (x[0]>'+str(mesh_P0)+') && (x[1]>'+str(mesh_P0)+') && (x[0]<'+str(mesh_LL)+') && (x[1]<'+str(mesh_DD)+') )'
+obsts  = 'on_boundary && (x[0]>'+str(mesh_P0)+') && (x[1]>'+str(mesh_P0)+') && (x[0]<'+str(mesh_LL)+') && (x[1]<'+str(mesh_DD)+')'
 outlet = '(x[0]=='+str(mesh_LL)+')'
 
-ds_inlet, ds_walls, ds_outlet = 1,2,3
-
-boundaries     = FacetFunction ('size_t', mesh)
-side_inlet     = CompiledSubDomain( inlet                )
-side_outlet    = CompiledSubDomain( outlet               )
-side_walls     = CompiledSubDomain( walls+' || '+obsts   )
-boundaries.set_all(0)
-side_inlet.mark   (boundaries, ds_inlet  )
-side_walls.mark   (boundaries, ds_walls  )
-side_outlet.mark  (boundaries, ds_outlet )
-ds = Measure( 'ds', subdomain_data=boundaries )
-
-FE_T  = TensorElement('P', 'triangle', 1)
-FE_V  = VectorElement('P', 'triangle', 1)
+FE_V  = VectorElement('P', 'triangle', 2)
 FE_P  = FiniteElement('P', 'triangle', 1)
 FE_A  = FiniteElement('P', 'triangle', 1)
 
-elem  = MixedElement([FE_V, FE_P, FE_T])
+elem  = MixedElement([FE_V, FE_V, FE_P, FE_P, FE_A])
 U     = FunctionSpace(mesh, elem)
-U_ten = FunctionSpace(mesh, FE_T)
 U_vel = FunctionSpace(mesh, FE_V)
 U_pre = FunctionSpace(mesh, FE_P)
 U_con = FunctionSpace(mesh, FE_A)
 
 ans = Function(U)
-u,p,t = split(ans)
+u1,u2,p1,p2,a1 = split(ans)
+v1,v2,q1,q2,b1 = TestFunctions(U)
 
 RH1   = Constant(cons_rh1)
 RH2   = Constant(cons_rh2)
@@ -64,30 +51,62 @@ MU1   = Constant(cons_mu1)
 MU2   = Constant(cons_mu2)
 N1    = Constant(1.0)
 N2    = Constant(2.0)
-N4    = Constant(4.0)
 
-u_inn  = Constant( (cons_vin, 0) )
-u_wal  = Constant( (0, 0) )
-t_out  = Constant( ((0,0),(0,0)) )
+a2    = N1 -a1
 
 G     = Constant( -cons_gg )
 GG    = as_vector([ Constant(0), G])
 
-F1    = MU1*inner(t,t)/N2 *dx \
-      + inner(grad(p), u) *dx
+def F_cct(u1,a1,u2,a2,tt):
+   return div( u1*a1 +u1*a1*a2 +u2*a2*a2) *tt *dx
 
-F2    = inner( grad(u)-t,grad(u)-t )   *dx \
-      + inner( u -u_inn,u -u_inn )     *ds(ds_inlet) \
-      + inner( u -u_wal,u -u_wal )     *ds(ds_walls) \
-      + inner( t -t_out,t -t_out )     *ds(ds_outlet)
+def F_mmt(u1,a1,u2,a2,p1,p2,mu1,mu2,rh1,rh2,tt):
+   return  inner( mu1*grad(u1)*a1,              grad(tt) )     *dx \
+         + inner( mu1*outer(u1,grad(a1) ),      grad(tt) )/N2  *dx \
+         + inner( mu1*grad(u1)*a1*a2,           grad(tt) )     *dx \
+         + inner( mu1*outer(u1,grad(a1*a2) ),   grad(tt) )/N2  *dx \
+         + inner( mu1*grad(u2)*a2*a2,           grad(tt) )/N2  *dx \
+         + inner( mu1*outer(u2,grad(a2*a2) ),   grad(tt) )/N2  *dx \
+         + inner( mu1*dot(grad(a1),grad(u1)),        tt  )/N2  *dx \
+         + inner( mu1*dot(grad(a1*a2),grad(u1)),     tt  )/N2  *dx \
+         + inner( mu2*grad(u2)*a1*a1,           grad(tt) )/N2  *dx \
+         + inner( mu2*dot(grad(a1*a1),grad(u2)),     tt  )/N2  *dx \
+         + inner( (grad(p1)+rh1*GG)*a1*(N1+a2),      tt  )     *dx \
+         + inner( (grad(p2)+rh2*GG)*a1*a1,           tt  )     *dx
 
-F = derivative(F1, ans)
-F = derivative(inner(F,F)+F2, ans, TestFunction(U))
+def F_eng(u1,a1,u2,a2,p1,p2,mu1,mu2,rh1,rh2,tt):
+   return  inner(  mu1*grad(u1),grad(u1) )/N2 *tt                       *dx \
+         + inner( -mu2*grad(u2),grad(u2) )/N2 *tt                       *dx \
+         + inner(  mu1*dot(u1,grad(u1).T)
+                  -mu2*dot(u2,grad(u2).T), grad(tt) )/N2                *dx \
+         + inner(  mu1*grad(u1),grad(u1)*(N1 -N2*a1) )/N2 *tt           *dx \
+         + inner( -mu2*grad(u2),grad(u2)*(N1 -N2*a1) )/N2 *tt           *dx \
+         + inner(  mu1*dot(u1,grad(u1).T)*(N1 -N2*a1)
+                  -mu2*dot(u2,grad(u2).T)*(N1 -N2*a1), grad(tt) )/N2    *dx \
+         + inner(  mu1*grad(u1),grad(u2)*(-N2 -N2*a1) )/N2 *tt          *dx \
+         + inner(  mu2*grad(u1),grad(u2)*(     N2*a1) )/N2 *tt          *dx \
+         + inner(  mu1*dot(u1,grad(u1).T), grad(tt)*(-N2 -N2*a1) )/N2   *dx \
+         + inner(  mu2*dot(u2,grad(u2).T), grad(tt)*(     N2*a1) )/N2   *dx \
+         + inner( (grad(p1)+rh1*GG),(u1-u2)*(N2 -N2*a1) )*tt            *dx \
+         + inner( (grad(p2)+rh2*GG),(u1-u2)*(    N2*a1) )*tt            *dx
 
-p_uu,p_pp = 0,1
+F  = F_cct(u1,a1,u2,a2,q1) \
+   + F_cct(u2,a2,u1,a1,q1) \
+   + F_mmt(u1,a1,u2,a2,p1,p2,MU1,MU2,RH1,RH2,v1) \
+   + F_mmt(u2,a2,u1,a1,p2,p1,MU2,MU1,RH2,RH1,v2) \
+   + F_eng(u2,a2,u1,a1,p2,p1,MU1,MU2,RH1,RH2,b1)
+
+u_in = Constant((cons_vin, 0))
+u_00 = Constant((0, 0))
+a_in = Constant(0.5)
+
+p_u1,p_u2,p_p1,p_p2,p_aa = 0,1,2,3,4
 BC = [
-      DirichletBC(U.sub(p_uu), u_in , inlet),
-      DirichletBC(U.sub(p_uu), u_00 , walls),
+      DirichletBC(U.sub(p_u1), u_in, inlet),
+      DirichletBC(U.sub(p_u2), u_00, inlet),
+      DirichletBC(U.sub(p_aa), a_in, inlet),
+      DirichletBC(U.sub(p_u1), u_00, walls),
+      DirichletBC(U.sub(p_u2), u_00, walls),
       # DirichletBC(U.sub(p_ux), Constant(0        ), obsts),
       # DirichletBC(U.sub(p_uy), Constant(0        ), obsts),
       # DirichletBC(U.sub(p_pp), Constant(0        ), outlet),
@@ -99,12 +118,12 @@ p_max = 1E2
 a_min = 0.0 +cons_tol
 a_max = 1.0 -cons_tol
 
-# lowBound = project(Constant((-v_max, -v_max, -v_max, -v_max, -p_max, -p_max, a_min)), U)
-# uppBound = project(Constant((+v_max, +v_max, +v_max, +v_max, +p_max, +p_max, a_max)), U)
+lowBound = project(Constant((-v_max, -v_max, -v_max, -v_max, -p_max, -p_max, a_min)), U)
+uppBound = project(Constant((+v_max, +v_max, +v_max, +v_max, +p_max, +p_max, a_max)), U)
 
 dF = derivative(F, ans)
 nlProblem = NonlinearVariationalProblem(F, ans, BC, dF)
-# nlProblem.set_bounds(lowBound,uppBound)
+nlProblem.set_bounds(lowBound,uppBound)
 nlSolver  = NonlinearVariationalSolver(nlProblem)
 nlSolver.parameters["nonlinear_solver"] = "snes"
 
@@ -115,7 +134,7 @@ prm["maximum_iterations"            ] = 15
 prm["maximum_residual_evaluations"  ] = 20000
 prm["absolute_tolerance"            ] = 8.0E-13
 prm["relative_tolerance"            ] = 6.0E-13
-# prm["method"                        ] = "vinewtonrsls" # vinewtonrsls, vinewtonssls
+prm["method"                        ] = "vinewtonrsls" # vinewtonrsls, vinewtonssls
 prm["linear_solver"                 ] = "mumps"
 # bicgstab       |  Biconjugate gradient stabilized method                      
 # cg             |  Conjugate gradient method                                   
@@ -128,7 +147,7 @@ prm["linear_solver"                 ] = "mumps"
 # superlu        |  SuperLU                                                     
 # tfqmr          |  Transpose-free quasi-minimal residual method                
 # umfpack        |  UMFPACK (Unsymmetric MultiFrontal sparse LU factorization)  
-# prm["line_search"                   ] = "basic" # "basic", "bt", "l2", "cp", "nleqerr"
+prm["line_search"                   ] = "basic" # "basic", "bt", "l2", "cp", "nleqerr"
 #prm["sign"                          ] = "default"
 #prm["preconditioner"                ] = "none"
 #prm["report"                        ] = True
@@ -137,16 +156,15 @@ prm["linear_solver"                 ] = "mumps"
 
 #set_log_level(PROGRESS)
 
-# a_init = Expression('0.5 +0.01*cos(x[0]*1000)+0.01*cos(x[1]*1000)', degree=2)
+a_init = Expression('0.5 +0.01*cos(x[0]*1000)+0.01*cos(x[1]*1000)', degree=2)
 
-# assign(ans.sub(p_u1 ), project(Constant((cons_vin*1.1,0)), FunctionSpace(mesh, FE_V) ) )
-# assign(ans.sub(p_u2 ), project(Constant((cons_vin*0.9,0)), FunctionSpace(mesh, FE_V) ) )
-# assign(ans.sub(p_p1 ), project(Constant(0.0E+0), FunctionSpace(mesh, FE_P) ) )
-# assign(ans.sub(p_p2 ), project(Constant(0.0E+0), FunctionSpace(mesh, FE_P) ) )
-# assign(ans.sub(p_aa ), project(a_init, FunctionSpace(mesh, FE_A) ) )
+assign(ans.sub(p_u1 ), project(Constant((cons_vin*1.1,0)), FunctionSpace(mesh, FE_V) ) )
+assign(ans.sub(p_u2 ), project(Constant((cons_vin*0.9,0)), FunctionSpace(mesh, FE_V) ) )
+assign(ans.sub(p_p1 ), project(Constant(0.0E+0), FunctionSpace(mesh, FE_P) ) )
+assign(ans.sub(p_p2 ), project(Constant(0.0E+0), FunctionSpace(mesh, FE_P) ) )
+assign(ans.sub(p_aa ), project(a_init, FunctionSpace(mesh, FE_A) ) )
 
-# nlSolver.solve()
-
+nlSolver.solve()
 # plot(u1, title='velocity')
 # plot(p1, title='pressure')
 # plot(a1, title='concentration')
